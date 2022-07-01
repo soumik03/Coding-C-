6c4b13251c5aa86332af04a36920cd75669af4d6
soumik03 <72907754+soumik03@users.noreply.github.com>
2022-07-01 23:58:19 +0530

    Create Program to find the sum of minimum absolute difference of the given array.cpp

---
+++ Program to find the sum of minimum absolute difference of the given array.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int sum=0;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    cin>>arr[i];
+    sort(arr,arr+n);
+    for(int i=0;i<n-1;i++)
+    sum+=abs(arr[i]-arr[i+1]);
+    int result=minDiff(arr);
+}
+
+
+// using vector
+/*int minDiff(vector<int> arr)
+{
+    int sum=0;
+     sort(arr.begin(), arr.end()); 
+      for (int i = 0; i < arr.size(); i++){
+      sum+=abs(arr[i]-arr[i+1]);
+      }
+      return sum;
+      
+}*/
